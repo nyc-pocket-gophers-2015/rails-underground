243269415101f6a20e7388c6b6b27cb43ed35cec
Steven Cassidy <steven@mumsnet.com>
2015-05-18 15:16:24 -0400

    Add some user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   has_secure_password
   has_many :events, foreign_key: 'organizer_id'
   has_many :reservations
