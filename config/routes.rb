dfa960b65dc061cfd97fbfdf10796469ffb21a0e
Steven Cassidy <steven@mumsnet.com>
2015-05-18 15:16:47 -0400

    Add a user signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resource :session, only:[:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   get 'login' => 'sessions#new'
+  get 'signup' => 'users#new'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
