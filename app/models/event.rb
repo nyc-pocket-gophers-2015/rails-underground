5449363511cc7d174cdb810a900610ffd323dee0
Steven Cassidy <steven@mumsnet.com>
2015-05-19 10:16:55 -0400

    Add future/past scopes to the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ActiveRecord::Base
   belongs_to :organizer, class_name: 'User'
   has_many :reservations
   has_many :guests, through: :reservations, source: :user
+
+  scope :future, -> { where('held_at > ?', Time.now) }
+  scope :past, -> { where('held_at < ?', Time.now) }
+
 end
