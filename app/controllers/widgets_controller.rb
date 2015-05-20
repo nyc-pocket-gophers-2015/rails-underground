b2bafdf13943e8d5bdf87017713013a173645f44
Steven Cassidy <steven@mumsnet.com>
2015-05-20 17:13:49 -0400

    Implement the widgets controller and spec

---
+++ app/controllers/widgets_controller.rb
@@ -0,0 +1,24 @@
+class WidgetsController < ApplicationController
+  def new
+    @widget = Widget.new
+  end
+
+  def index
+    @widgets = Widget.all
+  end
+
+  def create
+    @widget = Widget.new(widget_params)
+    if @widget.save 
+      flash[:notice] = "Widget was successfully created."
+      redirect_to widgets_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def widget_params
+    params.require(:widget).permit(:name)
+  end
+end
