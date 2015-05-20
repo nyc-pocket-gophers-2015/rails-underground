82bbbddcc53707d13636ffc466a525ac269d554f
Steven Cassidy <steven@mumsnet.com>
2015-05-20 10:10:40 -0400

    Add spec for widgets_controller create

--- spec/controllers/widgets_controller_spec.rb
+++ spec/controllers/widgets_controller_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe WidgetsController do
       expect(assigns(:widget)).to be_a_new Widget      
     end
   end
+
+  describe '#create' do
+    it "should create a widget" do
+      params = {widget:{name:'Steven'}}
+      expect{post(:create, params)}.to change{Widget.count}.by(1)
+    end
+    it "should redirect to widgets#index" do
+      params = {widget:{name:'Steven'}}
+      post(:create, params)
+      expect(response).to redirect_to(widgets_path)
+    end
+  end
 end
