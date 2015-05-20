c763492e1d01b494e363cd9b0b6e024cb34c39a2
Steven Cassidy <steven@mumsnet.com>
2015-05-20 10:14:24 -0400

    Pass the controller test for create

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -3,4 +3,19 @@ class WidgetsController < ApplicationController
     @widget = Widget.new
   end
 
+  def create
+    @widget = Widget.new(widget_params)
+    if @widget.save
+      redirect_to widgets_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def widget_params
+    params.require(:widget).permit(:name)
+  end
+
 end
